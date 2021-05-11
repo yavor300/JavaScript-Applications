b09a7b8153180216809dc6e31e26c012d7fac893
Yavor Chamov <yavor300@gmail.com>
2021-05-11 21:53:10 +0300

    01.Calendar

---
+++ 04-Single-Page-Applications/Lab/01.Calendar/src/app.js
@@ -0,0 +1,72 @@
+const monthNames = {
+    'Jan': 1,
+    'Feb': 2,
+    'Mar': 3,
+    'Apr': 4,
+    'May': 5,
+    'Jun': 6,
+    'Jul': 7,
+    'Aug': 8,
+    'Sep': 9,
+    'Oct': 10,
+    'Nov': 11,
+    'Dec': 12,
+}
+
+const yearSelect = document.querySelector('#years');
+
+const years = Array.from(document.querySelectorAll('.monthCalendar'))
+                .reduce((acc, c) => {
+                   acc[c.id] = c;
+                   return acc;
+                }, {});
+
+const months = Array.from(document.querySelectorAll('.daysCalendar'))
+    .reduce((acc, c) => {
+        acc[c.id] = c;
+        return acc;
+    }, {});
+
+function displaySection(section) {
+    document.body.innerHTML = '';
+    document.body.appendChild(section);
+}
+
+console.log(years);
+console.log(months)
+
+displaySection(yearSelect);
+
+yearSelect.addEventListener('click', (event) => {
+    if (event.target.classList.contains('date') || event.target.classList.contains('day')) {
+        event.stopImmediatePropagation();
+        const yearId = `year-${event.target.textContent.trim()}`;
+        displaySection(years[yearId]);
+    }
+});
+
+document.body.addEventListener('click', (event) => {
+   if (event.target.tagName === 'CAPTION') {
+       const sectionId = event.target.parentNode.parentNode.id;
+       if (sectionId.includes('year-')) {
+           displaySection(yearSelect);
+       } else if (sectionId.includes('month-')) {
+           const yearId = sectionId.split('-')[1];
+           displaySection(years[`year-${yearId}`]);
+       }
+   } else if (event.target.tagName === 'TD' || event.target.tagName === 'DIV') {
+       const monthName = event.target.textContent.trim();
+       if (monthNames.hasOwnProperty(monthName)) {
+           let parent = event.target.parentNode;
+           while (parent.tagName !== 'TABLE') {
+               parent = parent.parentNode;
+           }
+           const year = parent.querySelector('caption').textContent.trim();
+           const monthId = `month-${year}-${monthNames[monthName]}`;
+           displaySection(months[monthId]);
+       }
+   }
+});
+
+
+
