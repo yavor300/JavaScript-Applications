ee86dde80d1bc2872b6951ee33278262a8221a7b
Yavor Chamov <yavor300@gmail.com>
2021-07-11 09:10:32 +0300

    01.List-Towns

---
+++ 06-Client-Side-Rendering/Exercises/01.List-Towns/app.js
@@ -0,0 +1,23 @@
+import {html, render} from '../node_modules/lit-html/lit-html.js';
+
+const listTemplate = (data) => html`
+    <ul>
+        ${data.map(t => html`
+            <li>${t}</li>`)}
+    </ul>
+`;
+
+document.getElementById('btnLoadTowns').addEventListener('click', updateList);
+
+function updateList(event) {
+    event.preventDefault();
+
+    const townsAsString = document.getElementById('towns').value;
+    const root = document.getElementById('root');
+
+    const towns = townsAsString.split(',').map(x => x.trim());
+
+    const result = listTemplate(towns);
+
+    render(result, root);
+}
